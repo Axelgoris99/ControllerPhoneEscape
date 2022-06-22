4b3a875d19ffc76e14789af686a967de4d5ca1d4
Axel Goris <axelgoris99@gmail.com>
2022-06-22 18:46:30 +0200

    Feat: Working Client with Servers for multiple players - Tap Jump !

--- Assets/ClientAndServer/Script/Touch.cs
+++ Assets/ClientAndServer/Script/Touch.cs
@@ -1,16 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Touch : MonoBehaviour
 {
     public delegate void TapAction();
     public static event TapAction OnTap;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
